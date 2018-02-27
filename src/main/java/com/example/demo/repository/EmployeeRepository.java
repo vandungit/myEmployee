061039d0693e70b3cae9fe69a717fecf3bca3925
vandung1200vn <dungleevan08121998@gmail.com>
2018-02-28 01:13:53 +0700

    first commit

---
+++ src/main/java/com/example/demo/repository/EmployeeRepository.java
@@ -0,0 +1,13 @@
+package com.example.demo.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.demo.model.Employee;
+
+public interface EmployeeRepository extends CrudRepository<Employee, Integer> {
+
+    List<Employee> findByNameContaining(String q);
+
+}
